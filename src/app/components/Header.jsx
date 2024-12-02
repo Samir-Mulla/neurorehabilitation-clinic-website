a13d9fc2735b662b7e19ab0ddb7e5376bbba5537
Samir Mulla <samirmulla710@gmail.com>
2024-12-02 09:13:43 +0530

    Component : Header  is ready

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Logo from './Logo'
-import Navbar from './Navbar'
+"use client";
+
+import React from "react";
+import Navbar from "./Navbar";
+import ContactButton from "./ContactButton";
+// import Link from "next/link";
+
+const navItems = ["Home", "About Us", "Treatments", "Blog"];
 
 function Header() {
   return (
-    <header className='flex justify-around p-3'>
-        <Logo />
-        <Navbar />
+    <header className="flex flex-wrap justify-between items-center px-14 py-3 w-full text-black bg-[#DDF8FE] border border-black border-solid max-md:px-5">
+      <div className="font-extrabold text-sm lg:text-xl">Logo</div>
+      <Navbar navItems={navItems} />
+      <ContactButton text="Contact Us" href="/contact" />
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
